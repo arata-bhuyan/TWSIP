850d2a9f376d255e978badca22583aea4ed97011
arata-bhuyan <aratabhuyan360@gmail.com>
2024-04-28 12:57:22 +0530

    header & about

--- TaskOne/main.js
+++ TaskOne/main.js
@@ -14,3 +14,44 @@ navLinks.addEventListener("click",()=>{
     menuBtnIcon.setAttribute("class","ri-menu-line");
 });
 
+const scrollRevealOption ={
+    disttance:"50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+//header container
+ScrollReveal().reveal(".header__container p",{
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1",{
+    ...scrollRevealOption,
+    delay:500,
+});
+
+//about container
+ScrollReveal().reveal(".about__image img",{
+    ...scrollRevealOption,
+    origin:"left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader",{
+    ...scrollRevealOption,
+    delay:500,
+});
+
+ScrollReveal().reveal(".about__content .section__header",{
+    ...scrollRevealOption,
+    delay:1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description",{
+    ...scrollRevealOption,
+    delay:1500,
+});
+
+ScrollReveal().reveal(".about__btn",{
+    ...scrollRevealOption,
+    delay:2000,
+});
